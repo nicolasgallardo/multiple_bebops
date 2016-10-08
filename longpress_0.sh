08e369e12580207d4ecf4ec3cb1309c2237ec801
tnaegeli <naegelit@inf.ethz.ch>
2016-10-08 20:34:56 +0200

    update

--- longpress_0.sh
+++ longpress_0.sh
@@ -1,5 +1,5 @@
 mount -o remount,rw / &&
-wpa_passphrase 'AIT-2.4' 'hd9bq02ubn29u' > /etc/wpa_supplicant.conf &&  
+wpa_passphrase 'Networkname' 'password' > /etc/wpa_supplicant.conf &&  
 ifconfig eth0 192.168.1.102 && 
 wpa_supplicant -B -D wext -i eth0 -c /etc/wpa_supplicant.conf
 #wait 5 &&
